6031b12adb4656693cb00bb6946017952125a391
GhayaM <85023515+GhayaM@users.noreply.github.com>
2021-05-30 10:12:55 +0400

    Update first_arduino.ino

--- first_arduino/first_arduino.ino
+++ first_arduino/first_arduino.ino
@@ -1,12 +1,19 @@
 #include "M5Atom.h"
 
-bool IMU6886Flag = false;
+bool Flag = false;// changed IMU6886Flag to flag
 void setup()
 {
   M5.begin(true, false, true);
   if (!M5.IMU.Init()) IMU6886Flag = true;
 }
 
+void loop()
+{
+  float ax=0.0;ay=0.0,az=0.0;
+  if(Flag==true)
+  {
+    M4.IMU.getAcceIData(&ax,&ay,&az);
+  }
 uint8_t FSM = 0;
 bool onState = true;
 float triggered = 0;
@@ -27,28 +34,39 @@ float triggered = 0;
 
    switch (FSM)
   {
-    case 0:// should be off 
+    case 0:// OFF: All LEDs off
       for (int i = 0; i < 25; i++) {
         M5.dis.drawpix(i, 0x000000);
       }
       break;
 
-    case 1: // 
+    case 1: // Manual Rear strobe (RED): LEDs strobe at a predetermined interval
       for (int i = 0; i < 25; i++) {
         M5.dis.drawpix(i, onState ? 0x00f000 : 0x000000);
       }
       
       break;
       
-    case 2:
-   
+    case 2: //Manual Front strobe (WHITE): LEDs strobe at a predetermined interval
+     for (int i = 0; i < 25; i++) 
+      {
+        M5.dis.drawpix(i, onState ? 0xffffff: 0x000000);
+      }
       break;
 
-    case 3:
-   
+    case 3: //Automatic Rear Mode Rear (RED): LEDs are solid during a braking event. Return to strobe when riding.
+       for (int i = 0; i < 25; i++) 
+      {
+        M5.dis.drawpix(i, ax < 0.01 ? 0x00f000 : onState ? 0x00f000: 0x000000);
+      }
+        for (int i = 0; i < 25; i++) 
+      {
+        M5.dis.drawpix(i, ay > 0.01 ? 0x00f000 : onState ? 0x00f000: 0x000000);
+      }
       break;
 
-    case 4:
+    case 4: /
+      //Automatic Front Mode Rear (White): LEDs are solid during a braking event. Return to strobe when riding.
     break;
     }
     
@@ -56,5 +74,5 @@ float triggered = 0;
     triggered = millis();
     onState = !onState;
      }
-   
+}
       
